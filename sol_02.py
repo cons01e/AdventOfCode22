9534188a118d2b2765a606d130db14860f497e83
conso1e <menachemharrison@gmail.com>
2023-02-20 13:27:51 -0500

    cleaned up day 2 sol

--- sol_02.py
+++ sol_02.py
@@ -1,8 +1,7 @@
 # A -> rock, B -> paper, C -> scissors
-# X -> rock, Y -> paper, Z -> scissors
-#
-# part 1 score: ally_move + 3C[ally_move][enemy_move]
-# part 2 score: F(enemy_move, outcome) + 3C[F(enemy_move, outcome)][enemy_move]
+# Part 1: X -> rock, Y -> paper, Z -> scissors
+# Part 2: X -> loss, Y -> draw, Z -> win
+
 
 outcomes = [
     [0, 0, 0, 0],
@@ -10,23 +9,24 @@ outcomes = [
     [0, 2, 1, 0],
     [0, 0, 2, 1]
 ]
-fixed_outcomes = {
-    'AX': 3, 'AY': 1, 'AZ': 2,
-    'BX': 1, 'BY': 2, 'BZ': 3,
-    'CX': 2, 'CY': 3, 'CZ': 1
-}
-enemy_moves = {'A': 1, 'B': 2, 'C': 3}
-ally_moves = {'X': 1, 'Y': 2, 'Z': 3}
+fixed_outcomes = [
+    [0, 0, 0, 0],
+    [0, 3, 1, 2],
+    [0, 1, 2, 3],
+    [0, 2, 3, 1]
+]
+map_a = {'A': 1, 'B': 2, 'C': 3}
+map_b = {'X': 1, 'Y': 2, 'Z': 3}
 
 input_file = "./input/input_02.txt"
 with open(input_file) as file:
     lines = [line.rstrip() for line in file]
 
-score_a = 0
-score_b = 0
+scores = [0, 0]
 for line in lines:
-    enemy_move, ally_move = line.split()
-    score_a += ally_moves[ally_move] + 3 * outcomes[ally_moves[ally_move]][enemy_moves[enemy_move]]
-    score_b += fixed_outcomes[enemy_move + ally_move] + 3 * outcomes[fixed_outcomes[enemy_move + ally_move]][enemy_moves[enemy_move]]
+    a, b = line.split()
+    scores[0] += map_b[b] + 3 * outcomes[map_b[b]][map_a[a]]
+    scores[1] += fixed_outcomes[map_a[a]][map_b[b]] + 3 * outcomes[fixed_outcomes[map_a[a]][map_b[b]]][map_a[a]]
 
-print(f'part 1 score: {score_a}\npart 2 score: {score_b}')
+print(f'scores: {scores}')
+
