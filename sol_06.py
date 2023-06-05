be4af5b305e1c8a33a5a87f84661b0938d8ffc57
conso1e <menachemharrison@gmail.com>
2023-06-05 08:57:06 -0400

    day 6 part 2

--- sol_06.py
+++ sol_06.py
@@ -1,19 +1,23 @@
 # Advent of Code 2022
 # Day 6
+# todo: optimize
 
 
+import sys
 import lib
 
 
+part = int(sys.argv[1])
 # data = "bvwbjplbgvbhsrlpgdmjqwftvncz"
 full_data_path = "input/full/input_06.txt"
 lines = lib.read_input(full_data_path)
 
 data = lines[0]
-marker_pos = 3 # minimum eligible starting position
+marker_len = 4 if part == 1 else 14
+marker_pos = marker_len - 1 # minimum eligible starting position
 while marker_pos < len(data):
-    marker = data[marker_pos - 4:marker_pos]
-    if len(set(marker)) == 4:
+    marker = data[marker_pos - marker_len:marker_pos]
+    if len(set(marker)) == marker_len:
         break
     marker_pos += 1
 
