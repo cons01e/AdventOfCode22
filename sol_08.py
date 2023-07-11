ba3d2e7f90c90022a2e204f061823c2b64cfd1fc
conso1e <menachemharrison@gmail.com>
2023-07-10 21:40:05 -0400

    day 8 part 2

--- sol_08.py
+++ sol_08.py
@@ -2,7 +2,21 @@
 # Day 8
 
 import numpy
+import operator
+import functools
 import lib
+
+
+def directional_score(cell_val, subrange):
+    cell_val = int(cell_val)
+    if len(subrange) == 0:
+        return 0
+    #if cell_val <= int(subrange[0]):
+    #    return 1
+    for i, el in enumerate(subrange, 1):
+        if cell_val <= int(el):
+            break
+    return i
 
 
 sample_data_file = "input/sample/sample_08.txt"
@@ -67,3 +81,24 @@ for i, row in enumerate(M):
                 break
 
 print(N.sum())
+
+
+# part 2
+max_score = 0
+M = M.T
+for i, row in enumerate(M):
+    for j, cell_val in enumerate(row):
+        col = M[:, j]
+        subranges = [
+            row[j+1:],                  # right
+            list(reversed(row[:j])),    # left
+            col[i+1:],                  # down
+            list(reversed(col[:i]))     # up
+        ]
+        scores = [directional_score(cell_val, sr) for sr in subranges]
+        total_score = functools.reduce(operator.mul, scores, 1)
+        if total_score > max_score:
+            max_score = total_score
+
+print(max_score)
+
