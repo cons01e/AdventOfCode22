ff4057d1e2ec267948a58c5764d867a3f6f93e6a
conso1e <menachemharrison@gmail.com>
2023-07-16 22:24:41 -0400

    refactor: moved remaining functions into classes

--- sol_09.py
+++ sol_09.py
@@ -1,15 +1,24 @@
 # Advent of Code 2022
 # Day 9
+# todo: optimize tail_locations data structure
 
 
 import lib
 
+
+def sign(x):
+    return 1 if x > 0 else -1
 
 class KnotNode:
     def __init__(self, prv, nxt, pos):
         self.prv = prv
         self.nxt = nxt
         self.pos = pos
+
+    def distance(self, knot):
+        dx = knot.pos[0] - self.pos[0]
+        dy = knot.pos[1] - self.pos[1]
+        return (dx, dy)
 
 class Rope:
     def __init__(self, size):
@@ -23,38 +32,38 @@ class Rope:
             knot = knot.nxt
         return rope
 
-def sign(x):
-    return 1 if x > 0 else -1
+    def move_head(self, direction):
+        match direction:
+            case "R":
+                self.head.pos[0] += 1
+            case "L":
+                self.head.pos[0] -= 1
+            case "D":
+                self.head.pos[1] += 1
+            case "U":
+                self.head.pos[1] -= 1
+        self.move_knot(self.head, self.head.nxt)
 
-def distance(head, tail):
-    dx = head[0] - tail[0]
-    dy = head[1] - tail[1]
-    return (dx, dy)
+    def move_knot(self, prev, curr):
+        move_next = True
+        dx, dy = curr.distance(prev)
 
-def move_head(head_pos, direction):
-    match direction:
-        case "R":
-            head_pos[0] += 1
-        case "L":
-            head_pos[0] -= 1
-        case "D":
-            head_pos[1] += 1
-        case "U":
-            head_pos[1] -= 1
+        if abs(dx) + abs(dy) > 2:   # diagonally
+            curr.pos[0] += 1 * sign(dx)
+            curr.pos[1] += 1 * sign(dy)
+        elif abs(dx) > 1:           # horizonally
+            curr.pos[0] += 1 * sign(dx)
+        elif abs(dy) > 1:           # vertially
+            curr.pos[1] += 1 * sign(dy)
+        else:
+            move_next = False
 
-def move_knot(prev_pos, curr, visited_spaces):
-    dx, dy = distance(prev_pos, curr.pos)
-    if abs(dx) + abs(dy) > 2:   # diagonal move
-        curr.pos[0] += 1 * sign(dx)
-        curr.pos[1] += 1 * sign(dy)
-    elif abs(dx) > 1:   # horizonal move
-        curr.pos[0] += 1 * sign(dx)
-    elif abs(dy) > 1:   # vertial move
-        curr.pos[1] += 1 * sign(dy)
-    else:
-        pass # optimeize by breaking here
-    if curr.nxt is None:
-        visited_spaces.add(tuple(curr.pos))
+        if curr.nxt is None:
+            self.tail_locations.add(tuple(curr.pos))
+            move_next = False
+
+        if move_next:
+            self.move_knot(curr, curr.nxt)
 
 
 sample_data = lib.read_input("input/sample/sample_09.txt")
@@ -66,14 +75,9 @@ for line in full_data:
     direction, moves = line.split(" ")
     moves = int(moves)
 
-    while moves:
+    for _ in range(moves):
         for rope in ropes:
-            move_head(rope.head.pos, direction)
-            knot = rope.head.nxt
-            while knot is not None:
-                move_knot(knot.prv.pos, knot, rope.tail_locations)
-                knot = knot.nxt
-        moves -= 1
+            rope.move_head(direction)
 
 print(len(ropes[0].tail_locations), len(ropes[1].tail_locations))
 
